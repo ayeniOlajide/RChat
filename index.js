782e8794883963887b4d9a93eaad8f2184d32a59
MACBOOK <macbook@MacBook-Pro.local>
2024-06-21 20:08:27 +0100

    changes

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.post('/chat', (req, res) => {
   res.sendStatus(200);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const HOST = '127.0.0.1';
 
 server.listen(PORT, HOST, () => {
